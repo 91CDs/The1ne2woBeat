aeb74e7dcbf7923746a05d80f9d697b65905d5a2
C_D_91 <benamercarl@gmail.com>
2022-09-19 20:14:56 +0400

    added icon and remove light mode

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,43 @@
 <script setup lang="ts">
+import { onMounted, onUnmounted, ref } from "vue";
 import { RouterLink, RouterView } from "vue-router";
+import HamburgerIcon from "./components/Icons/HamburgerIcon.vue";
 function isMobile() {
   const width = window.innerWidth;
   return width <= 1024 ? true : false;
 }
+const isMenuDisplay = ref<boolean>(false);
+function setMenuDisplay(this: HTMLElement, event: Event) {
+  const el = event.target as HTMLElement;
+  if (el == this) {
+    isMenuDisplay.value = !isMenuDisplay.value;
+  }
+}
+onMounted(() => {
+  const menubtn = document.querySelector(".menubtn");
+  const menu = document.querySelector(".menu");
+  if (menubtn && menu) {
+    console.log(menubtn);
+    menubtn.addEventListener("click", setMenuDisplay);
+    menu.addEventListener("click", setMenuDisplay);
+  }
+});
+onUnmounted(() => {
+  const menubtn = document.querySelector(".menubtn");
+  if (menubtn) {
+    menubtn.removeEventListener("click", setMenuDisplay);
+  }
+});
 </script>
 
 <template>
   <header>
     <nav v-if="isMobile()">
       <img class="logo" src="/logo.png" />
-      <i class="right menubtn fas fa-bars"></i>
-      <div class="menu">
+      <HamburgerIcon />
+      <div class="menu" v-show="isMenuDisplay">
         <img class="profile" src="/pfp.jpg" />
+        <RouterLink to="/"> <i class="fas fa-house icon"></i>Home </RouterLink>
         <RouterLink to="/search">
           <i class="fas fa-magnifying-glass icon"></i>Search
         </RouterLink>
@@ -68,20 +93,20 @@ nav a:first-of-type {
   border: 0;
 }
 .menubtn {
-  padding: 0 1rem;
+  padding: 1rem;
 }
 .menu {
   display: flex;
   flex-direction: column;
   height: 100vh;
+  width: 100vw;
   position: fixed;
   top: 0;
   right: 0;
   z-index: 10;
-  padding: 50% 0;
-  background-color: rgba(24, 24, 24, 0.5);
-  backdrop-filter: blur(5px);
-  width: 50vw;
+  padding: 30vh 0;
+  background-color: rgba(24, 24, 24, 0.6);
+  backdrop-filter: blur(10px);
 }
 .menu .profile {
   order: 0;
@@ -108,6 +133,8 @@ nav a:first-of-type {
   nav {
     font-size: 1rem;
     text-align: left;
+  }
+  nav a {
     border-left: 1px solid var(--color-border);
   }
   .settings {
